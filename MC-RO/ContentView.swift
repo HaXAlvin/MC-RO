3d74d012229c75d8cfe9a1bf4e5a92bff34887fd
HaXAlvin <qazqwe0922@gmail.com>
2020-07-15 20:24:09 +0800

    first

---
+++ MC-RO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MC-RO
+//
+//  Created by 薛竣祐 on 2020/7/15.
+//  Copyright © 2020 薛竣祐. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
